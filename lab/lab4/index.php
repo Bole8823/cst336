ef0d4a19abca82583b95ae12f56393a1fcca8688
Berenice Bole <bbole@csumb.edu>
2018-03-26 15:47:23 +0000

    new

--- lab/lab4/index.php
+++ lab/lab4/index.php
@@ -74,7 +74,22 @@
         
             <?php
         
-            $bdd = new PDO("mysql:host=localhost;dbname=lab4", 'bole8823', '');
+//$connUrl = getenv('JAWSDB_MARIA_URL');
+$connUrl = "mysql://pywo8h8n9lylz83t:xzlg0tzxs8w1ndax@jlg7sfncbhyvga14.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/mhq1uy937tuxjh65";
+$hasConnUrl = !empty($connUrl);
+
+$connParts = null;
+if ($hasConnUrl) {
+    $connParts = parse_url($connUrl);
+}
+
+//var_dump($hasConnUrl);
+$host = $hasConnUrl ? $connParts['host'] : getenv('IP');
+$dbname = $hasConnUrl ? ltrim($connParts['path'],'/') : 'crime_tips';
+$username = $hasConnUrl ? $connParts['user'] : getenv('C9_USER');
+$password = $hasConnUrl ? $connParts['pass'] : '';
+
+$bdd = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
             
             if ($_GET['type']!='')
             {
@@ -141,13 +156,8 @@
                     <strong>Device: </strong> <?php echo $donnees['deviceName']; ?><br />
                     <strong>type : </strong><?php echo $donnees['deviceType']; ?> <br />
                     <strong>Price: </strong>$<?php echo $donnees['price']; ?> <br />
-                    <?php
-                    if($donnees['status']=='CheckedOut' || $donnees['status']=='Checkedout'){
-                        echo '<span style="color:red;text-align:center;"><strong>Status: </strong> Checked Out</span>';
-                    } else {
-                        echo '<span style="color:green;text-align:center;"><strong>Status: </strong> Available</span>';
-                    }
-                    ?>
+                    <strong>Status: </strong><?php echo $donnees['status']; ?> <br />
+                    
                     
                 
                 </div>
