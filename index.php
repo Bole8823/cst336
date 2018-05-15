38e1ccd1f12bd21672747c163317e6c9d4725852
Berenice Bole <bbole@csumb.edu>
2018-05-15 06:43:19 +0000

    add

--- index.php
+++ index.php
@@ -16,13 +16,39 @@
             
         
         <main>
-            <h1>FINAL</h1>
-            <a href="final/2018.05.14">Final try 2</a> </br>
+            <h1>Challenge</h1>
+            <a href="examples/number">guess number </a> </br>
+            <a href="examples/login">login</a> </br></br>
+            <a href="examples/news">News</a> </br></br>
+            <a href="examples/scheduler">S</a> </br></br>
+            <h1>Homework</h1>
+            <a href="homework/homework1">Homework 1</a> </br>
+            <a href="homework/homework2">Homework 2</a> </br></br>
+            <h1>Lab</h1> 
+            <a href="lab/lab1">lab 1</a> </br>
+            <a href="lab/Lab3">Lab3</a> <p1> : this is the copy of the lab in my respository.</p1> </br>
+            <p1>the original work is here : </p1>  <a href="https://github.com/Bole8823/lab3">lab3</a></br>
+            <a href="lab/lab4">Lab 4</a> </br>
+            <a href="lab/lab5">Lab 5 !</a> </br>
+            
+           
+            
+            </br></br>
+             <h1>Assignment</h1>
+            <a href="assignment/assignment4">Assignment 4</a> </br>
             
             
+            </br></br></br>
+            <a href="midterm">midterm</a> </br>
+            </br></br></br>
+            <h1>Team Project</h1>
+            <a href="database_project">team project - database</a> </br>
             
+            </br></br>
+            <a href="final">Final</a>
+            </br></br>
             
-            
+        
         </main>
         
         
